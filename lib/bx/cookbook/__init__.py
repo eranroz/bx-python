1083da3c0a72e829902c0f54170cc1c414fc81ca
eranroz <eranroz89@gmail.com>
2016-11-16 23:03:48 +0200

    python3 support

--- lib/bx/cookbook/__init__.py
+++ lib/bx/cookbook/__init__.py
@@ -83,16 +83,16 @@ class ImmutableDict(dict):
     def __init__(self,*args,**kwds):
         dict.__init__(self,*args,**kwds)
     def __setitem__(self,key,value):
-        raise NotImplementedError, "dict is immutable"
+        raise NotImplementedError("dict is immutable")
     def __delitem__(self,key):
-        raise NotImplementedError, "dict is immutable"
+        raise NotImplementedError("dict is immutable")
     def clear(self):
-        raise NotImplementedError, "dict is immutable"
+        raise NotImplementedError("dict is immutable")
     def setdefault(self,k,default=None):
-        raise NotImplementedError, "dict is immutable"
+        raise NotImplementedError("dict is immutable")
     def popitem(self):
-        raise NotImplementedError, "dict is immutable"
+        raise NotImplementedError("dict is immutable")
     def update(self,other):
-        raise NotImplementedError, "dict is immutable"
+        raise NotImplementedError("dict is immutable")
     def __hash__(self):
         return hash(tuple(self.iteritems()))
